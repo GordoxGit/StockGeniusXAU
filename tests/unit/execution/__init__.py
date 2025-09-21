534a2d4bc2aacd15ad22cbe6fb6482c9a2aafc2c
GordoxGit <romain.begot@gmail.com>
2025-09-21 14:02:40 +0200

    Delete tests directory

--- tests/unit/execution/__init__.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import pytest
-
-if sys.platform != "win32" or sys.version_info >= (3, 12):
-    pytest.skip(
-        "Tests MT5 ignorés hors Windows ou Python>=3.12",
-        allow_module_level=True,
-    )
